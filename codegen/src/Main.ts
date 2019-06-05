303712e4087447a124b4e9058ac0871597064c2e
Dustin Whitney <dustin.whitney@gmail.com>
2019-06-05 12:18:03 -0400

    latest

--- codegen/src/Main.ts
+++ codegen/src/Main.ts
@@ -3,151 +3,265 @@ import * as ts from "typescript"
 export type Member = { name: string, type: TSType, optional: boolean }
 
 export type TSType = 
-  BooleanType |
-  StringType |
-  NumberType |
-  NullType |
-  AnyType |
-  VoidType |
-  StringLiteralType |
-  NumericLiteralType |
-  UnionType |
-  FunctionType |
-  TypeReference |
   AnonymousObject |
+  AnyType |
+  BooleanType |
+  ExceptionType |
+  FunctionType |
   InterfaceReference |
-  UnknownObject |
+  NullType |
+  NumberType |
+  NumericLiteralType |
+  StringLiteralType |
+  StringType |
+  TupleType |
+  TypeAlias |
   TypeParam |
+  TypeReference |
+  UnionType |
   Unknown |
-  TypeAlias
+  UnknownObject |
+  VoidType 
 
-export interface BooleanType { _tag: "Boolean" }
-export interface StringType { _tag: "String" }
-export interface NumberType { _tag: "Number" }
-export interface NullType { _tag: "Null" }
-export interface VoidType { _tag: "Unit" }
-export interface AnyType { _tag: "Any" }
-export interface UnionType { _tag: "Union", types: TSType[] }
-export interface StandardType { _tag: "Standard", type: string }
-export interface StringLiteralType { _tag: "StringLiteral", value: string }
-export interface NumericLiteralType { _tag: "NumericLiteral", value: number }
-export interface FunctionType { _tag: "Function", parameters: Member[], returnType?: TSType }
-export interface TypeReference { _tag: "TypeReference", name: string, typeParams: TSType[], flags: ts.TypeFlags, objFlags: ts.ObjectFlags }
-export interface AnonymousObject { _tag: "AnonymousObject", members: any }
-export interface InterfaceReference { _tag: "InterfaceReference", name: string }
-export interface UnknownObject { _tag: "UnknownObject", flags: ts.ObjectFlags }
-export interface TypeParam { _tag: "TypeParam", name: string }
-export interface Unknown { _tag: "Unknown", name: string, flags: ts.TypeFlags }
-export interface TypeAlias { _tag: "TypeAlias", typeReference: string, typeParams: TSType[], type: TSType }
+export interface BooleanType { readonly tag: "Boolean" }
+
+export interface StringType { readonly tag: "String" }
+
+export interface NumberType { readonly tag: "Number" }
+
+export interface NullType { readonly tag: "Null" }
+
+export interface VoidType { readonly tag: "Unit" }
+
+export interface AnyType { readonly tag: "Any" }
+
+export interface UnionType {
+  readonly tag: "Union",
+  readonly contents: { 
+    readonly types: TSType[] 
+  } 
+}
+
+export interface StandardType { 
+  readonly tag: "Standard",
+  readonly contents: { 
+    readonly type: string 
+  } 
+}
+
+export interface StringLiteralType {
+  readonly tag: "StringLiteral",
+  readonly contents: {
+    readonly value: string
+  }
+}
+
+export interface NumericLiteralType {
+  readonly tag: "NumericLiteral",
+  readonly contents: {
+    readonly value: number
+  }
+}
+
+export interface FunctionType {
+  readonly tag: "Function",
+  readonly contents: {
+    readonly parameters: Member[],
+    readonly returnType?: TSType
+  }
+}
+
+export interface TypeReference {
+  readonly tag: "TypeReference",
+  readonly contents: {
+    readonly name: string,
+    readonly typeParams: TSType[],
+    readonly flags: ts.TypeFlags,
+    readonly objFlags: ts.ObjectFlags
+  }
+}
+
+export interface AnonymousObject {
+  readonly tag: "AnonymousObject",
+  readonly contents: {
+    readonly members: Member[]
+  }
+}
+
+export interface InterfaceReference {
+  readonly tag: "InterfaceReference",
+  readonly contents: { readonly name: string }
+}
+
+export interface UnknownObject {
+  readonly tag: "UnknownObject",
+  readonly contents: {
+    readonly flags: ts.ObjectFlags 
+  }
+}
+
+export interface TypeParam {
+  readonly tag: "TypeParam",
+  readonly contents: {
+    readonly name: string
+  }
+}
+
+export interface Unknown {
+  readonly tag: "Unknown",
+  readonly contents: {
+    readonly name: string,
+    readonly flags: ts.TypeFlags
+  }
+}
+
+export interface TypeAlias {
+  readonly tag: "TypeAlias",
+  readonly contents: {
+    readonly typeReference: string,
+    readonly typeParams: TSType[],
+    readonly type: TSType
+  }
+}
+
+export interface TupleType {
+  readonly tag: "TupleType",
+  readonly contents: {
+    readonly types: TSType[]
+  }
+}
+
+export interface ExceptionType {
+  readonly tag: "ExceptionType",
+  readonly contents: {
+    readonly e: Error,
+    readonly type: string,
+    readonly flags: ts.TypeFlags
+  }
+}
 
 
-
-const options = ts.getDefaultCompilerOptions()
-const program = ts.createProgram(["./node_modules/@material-ui/core/index.d.ts"], options)
-const sources = program.getSourceFiles()
-const checker = program.getTypeChecker()
-
-
-const isNodeExported = (node: ts.Node): boolean => 
-  (ts.getCombinedNodeFlags(node) & ts.ModifierFlags.Export) !== 0 ||
-    (!!node.parent && node.parent.kind === ts.SyntaxKind.SourceFile)
-
-
-const getTSType = (type: ts.Type): TSType => {
-  if(type.isUnionOrIntersection()) return { _tag: "Union", types: (type.types.map(getWithAliasProps).filter(t => t) as unknown as TSType[]) }
-  if(type.flags & ts.TypeFlags.String) return { _tag: "String" }
-  if(type.flags & ts.TypeFlags.BooleanLike) return { _tag: "Boolean" }
-  if(type.flags & ts.TypeFlags.Number) return { _tag: "Number" }
-  if(type.flags & ts.TypeFlags.Null) return { _tag: "Null" }
-  if(type.flags & ts.TypeFlags.VoidLike) return { _tag: "Unit" }
-  if(type.flags & ts.TypeFlags.Any) return { _tag: "Any" }
-  if(type.isStringLiteral()) return { _tag: "StringLiteral", value: type.value }
-  if(type.isNumberLiteral()) return { _tag: "NumericLiteral", value: type.value }
-
-  const callSigs = type.getCallSignatures()
+const readTypes = (): TSType[] => {
   
-  if(callSigs.length){
-    const sig = callSigs[0] 
-    const parameters = sig.getParameters().map(p => optionalMember(p))
-    const returnType = getWithAliasProps(sig.getReturnType())
-    return { _tag: "Function", parameters, returnType }
-  }
+  const output: TSType[] = []
 
-  if(type.flags & ts.TypeFlags.Object | ts.TypeFlags.NonPrimitive){
-    const objFlags = (<ts.ObjectType>type).objectFlags
-    
-    if(objFlags & ts.ObjectFlags.Reference){
-      try{
-        const tr = (<ts.TypeReference>type)
-        const name = checker.getFullyQualifiedName(type.symbol)
-        const typeParams = tr.typeArguments ? (tr.typeArguments.map(getWithAliasProps).filter(i => i) as unknown as TypeAlias[]) : ([] as TypeAlias[])
-        const flags = type.flags
-        return { _tag: "TypeReference", name, typeParams, flags, objFlags }
-      } catch (e) {
-        console.error("Reference didn't work", checker.typeToString(type))
-        return { _tag: "Unknown", name: checker.typeToString(type), flags: type.flags }
+  const getTSType = (type: ts.Type): TSType => {
+    try{
+      if(type.isUnionOrIntersection()) return { tag: "Union", contents: { types: (type.types.map(getWithAliasProps).filter(t => t) as unknown as TSType[]) } }
+      if(type.flags & ts.TypeFlags.String) return { tag: "String" }
+      if(type.flags & ts.TypeFlags.BooleanLike) return { tag: "Boolean" }
+      if(type.flags & ts.TypeFlags.Number) return { tag: "Number" }
+      if(type.flags & ts.TypeFlags.Null) return { tag: "Null" }
+      if(type.flags & ts.TypeFlags.VoidLike) return { tag: "Unit" }
+      if(type.flags & ts.TypeFlags.Any) return { tag: "Any" }
+      if(type.isStringLiteral()) return { tag: "StringLiteral", contents: { value: type.value } }
+      if(type.isNumberLiteral()) return { tag: "NumericLiteral", contents: { value: type.value } }
+  
+      const callSigs = type.getCallSignatures()
+      
+      if(callSigs.length){
+        const sig = callSigs[0] 
+        const parameters = sig.getParameters().map(p => optionalMember(p))
+        const returnType = getWithAliasProps(sig.getReturnType())
+        return { tag: "Function", contents: { parameters, returnType } }
+      }
+  
+      if(type.flags & (ts.TypeFlags.Object | ts.TypeFlags.NonPrimitive)){
+        const objFlags = (<ts.ObjectType>type).objectFlags
+  
+        if(objFlags & ts.ObjectFlags.Tuple){
+          const types: TSType[] = []
+          return { tag: "TupleType", contents: { types } }
+        }
+        
+        if(objFlags & ts.ObjectFlags.Anonymous){
+          const members = convertProperties(type)
+          return { tag: "AnonymousObject", contents: { members } }
+        }
+  
+        if(objFlags & ts.ObjectFlags.Reference){
+          const tr = (<ts.TypeReference>type)
+          const name = checker.getFullyQualifiedName(type.symbol)
+          const typeParams = tr.typeArguments ? (tr.typeArguments.map(getWithAliasProps).filter(i => i) as unknown as TypeAlias[]) : ([] as TypeAlias[])
+          const flags = type.flags
+          return { tag: "TypeReference", contents: { name, typeParams, flags, objFlags } }
+        }
+  
+        if(objFlags & ts.ObjectFlags.Interface){
+          const name = checker.getFullyQualifiedName(type.symbol) 
+          return { tag: "InterfaceReference", contents: { name } }
+        }
+  
+        return { tag: "UnknownObject", contents: { flags: objFlags } }
+  
+      }
+  
+      if(type.flags & ts.TypeFlags.TypeParameter){
+        const name = checker.typeToString(type)
+        return { tag: "TypeParam", contents: { name } }
+      }
+  
+      return { tag: "Unknown", contents: { name: checker.typeToString(type), flags: type.flags } }
+  
+    } catch (e) {
+      console.log(checker.typeToString(type))
+      return { tag: "ExceptionType", contents: { e, type: checker.typeToString(type), flags: type.flags } }
+    }
+  
+  }
+  
+  const getWithAliasProps = (t: ts.Type): TSType => {
+    const type = getTSType(t)
+    if(t.aliasSymbol){
+      const typeReference = checker.getFullyQualifiedName(t.aliasSymbol)
+      const typeParams = t.aliasTypeArguments ? t.aliasTypeArguments.map(getTSType) : []
+      return { tag: "TypeAlias", contents: { typeReference, typeParams, type } }
+    }
+    return type
+  }
+  
+  
+  const optionalMember = (sym: ts.Symbol, node?: ts.Node): Member => {
+    const optional = ((sym.flags & ts.SymbolFlags.Optional) === ts.SymbolFlags.Optional)
+    const type = getWithAliasProps(checker.getTypeOfSymbolAtLocation(sym, node ? node : sym.valueDeclaration))
+    return { name: sym.name, type, optional }
+  }
+  
+  const convertProperties = (nodeType: ts.Type, node?: ts.Node): Member[] => 
+    nodeType.getProperties().map((sym: ts.Symbol) => optionalMember(sym, node))
+
+  const isNodeExported = (node: ts.Node): boolean => 
+    (ts.getCombinedNodeFlags(node) & ts.ModifierFlags.Export) !== 0 ||
+      (!!node.parent && node.parent.kind === ts.SyntaxKind.SourceFile)
+ 
+  const visit = (node: ts.Node) => {
+    if(isNodeExported(node)){
+      if(ts.isInterfaceDeclaration(node)){
+        const symbol = checker.getSymbolAtLocation(node.name)
+        if(symbol){
+          const nodeType = checker.getTypeAtLocation(node)
+          if(nodeType.isClassOrInterface()){
+            let members = convertProperties(nodeType, node)
+          }
+        }
+      } else if(ts.isModuleDeclaration(node)){
+        ts.forEachChild(node, visit)
       }
     }
-
-    if(objFlags & ts.ObjectFlags.Anonymous){
-      const members = convertProperties(type)
-      return { _tag: "AnonymousObject", members }
-    }
-
-    if(objFlags & ts.ObjectFlags.Interface){
-      const name = checker.getFullyQualifiedName(type.symbol) 
-      return { _tag: "InterfaceReference", name }
-    }
-
-    return { _tag: "UnknownObject", flags: objFlags }
-
   }
+  
+  const options = ts.getDefaultCompilerOptions()
+  const program = ts.createProgram(["./node_modules/@material-ui/core/index.d.ts"], options)
+  const sources = program.getSourceFiles()
+  const checker = program.getTypeChecker()
+  
+  sources.forEach(source => {
+    ts.forEachChild(source, visit)
+  })
 
-  if(type.flags & ts.TypeFlags.TypeParameter){
-    const name = checker.typeToString(type)
-    return { _tag: "TypeParam", name }
-  }
-
-  return { _tag: "Unknown", name: checker.typeToString(type), flags: type.flags }
-
+  return output
 }
 
-const getWithAliasProps = (t: ts.Type): TSType => {
-  const type = getTSType(t)
-  if(t.aliasSymbol){
-    const typeReference = checker.getFullyQualifiedName(t.aliasSymbol)
-    const typeParams = t.aliasTypeArguments ? t.aliasTypeArguments.map(getTSType) : []
-    return { _tag: "TypeAlias", typeReference, typeParams, type }
-  }
-  return type
-}
-
-
-const optionalMember = (sym: ts.Symbol, node?: ts.Node): Member => {
-  const optional = ((sym.flags & ts.SymbolFlags.Optional) === ts.SymbolFlags.Optional)
-  const type = getWithAliasProps(checker.getTypeOfSymbolAtLocation(sym, node ? node : sym.valueDeclaration))
-  return { name: sym.name, type, optional }
-}
-
-const convertProperties = (nodeType: ts.Type, node?: ts.Node): any => 
-  nodeType.getProperties().map((sym: ts.Symbol) => optionalMember(sym, node))
-
-const visit = (node: ts.Node) => {
-  if(ts.isInterfaceDeclaration(node)){
-    const symbol = checker.getSymbolAtLocation(node.name)
-    if(symbol){
-      const nodeType = checker.getTypeAtLocation(node)
-      if(nodeType.isClassOrInterface()){
-        let members = convertProperties(nodeType, node)
-      }
-    }
-  } else if(ts.isModuleDeclaration(node)){
-    ts.forEachChild(node, visit)
-  }
-}
-
-sources.forEach( source => {
-  ts.forEachChild(source, visit)
-})
+readTypes()
 
 
