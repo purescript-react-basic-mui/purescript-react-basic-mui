7b72666703811b18f44fbbbc466736d79ca6a48a
Dustin Whitney <dustin.whitney@gmail.com>
2019-06-13 13:08:08 -0400

    writing files

--- codegen/src/Write/React2.purs
+++ codegen/src/Write/React2.purs
@@ -3,21 +3,27 @@ module Codegen.Write.React2 where
 import Prelude
 
 import Codegen.Read (DeclarationElements(..), DeclarationSourceFile(..), EntityName(..), TSType(..), TypeReferenceRec, VariableDeclaration(..), _PropertySignature, _VariableDeclaration, _type, liftEither, toArrayOf, typescript)
+import Data.Array (fold)
 import Data.Array as Array
 import Data.Lens (_Just, preview, traversed)
 import Data.Lens.Index (ix)
-import Data.Maybe (Maybe(..), isJust, isNothing, maybe)
+import Data.Maybe (Maybe(..), fromMaybe, isJust, isNothing, maybe)
 import Data.String as String
 import Data.String.Regex (Regex)
 import Data.String.Regex as Regex
 import Data.String.Regex.Flags as RegexFlags
-import Data.Traversable (traverse_)
+import Data.Traversable (sequence, traverse, traverse_)
+import Debug.Trace (spy)
 import Effect (Effect)
 import Effect.Console (log)
 import Foreign (Foreign)
 import Foreign.Object (Object)
 import Foreign.Object as Object
+import Node.Encoding (Encoding(..))
+import Node.FS.Sync as FS
 import Node.Path (FilePath)
+import Node.Path as Path
+import Node.Process as Process
 import Run (Run, EFFECT)
 import Run as Run
 import Run.Reader (READER)
@@ -30,6 +36,7 @@ type Context =
   , sources :: Array DeclarationSourceFile 
   , types :: Object Foreign
   , regexps :: Regexps
+  , outputRoot :: String
   }
 type State = {
   files :: Object PSJS
@@ -37,6 +44,8 @@ type State = {
 
 type Regexps = 
   { javascriptImport :: Regex
+  , codegenPath :: Regex
+  , firstChar :: Regex
   }
 
 type ReactWriter a = Run(reader :: READER Context, state :: STATE State, effect :: EFFECT) a
@@ -52,50 +61,77 @@ data TypeReplacement
 
 type PureScript = 
   { imports :: Array String
+  , moduleName :: String
+  , path :: FilePath
   , foreignImports :: Array String
   , elements :: Array TSType
   }
-type JavaScript = { imports :: Array String , exports :: Array String }
+type JavaScript = { path :: FilePath, imports :: Array String , exports :: Array String }
 type PSJS = { path :: FilePath, javascript :: JavaScript, purescript :: PureScript }
 type FileDescription = { fileName :: String, text :: String }
 
-writeJavascript :: ReactWriter Unit
-writeJavascript = do
-  { files } <- State.get
-  traverse_ writeJS files
-  where
-    writeJS :: PSJS -> ReactWriter Unit 
-    writeJS { path, javascript } = do
-      Run.liftEffect $ traverse_ (\i -> log (path <> "\n" <> i)) javascript.imports
-      pure unit
+ 
  
 main :: Effect Unit
 main = do
   regex                 <- liftEither $ Regex.regex ".*Card.*" RegexFlags.noFlags
   javascriptImport      <- liftEither $ Regex.regex ".*(@material-ui.*)\\/(.*\\.d\\.ts)" RegexFlags.noFlags
-  let regexps           =  { javascriptImport }
+  codegenPath           <- liftEither $ Regex.regex ".*@material-ui.*\\/core\\/(.*)\\.d\\.ts" RegexFlags.noFlags
+  firstChar             <- liftEither $ Regex.regex "^(.)" RegexFlags.noFlags
+  let regexps           =  { javascriptImport, codegenPath, firstChar }
   let path              =  "./node_modules/@material-ui/core/index.d.ts"
+  cwd                   <- Process.cwd
+  outputRoot            <- Path.resolve [cwd] "../src/"
   { sources, types }    <- typescript path regex
-  let filteredSources = Array.filter (\(DeclarationSourceFile { fileName }) -> isNothing $ String.indexOf (String.Pattern "index.d.ts") fileName) sources
-  let context = { sources : filteredSources, types, typeReplacements, regexps }
-  let state = mempty
+  let filteredSources   =  Array.filter (\(DeclarationSourceFile { fileName }) -> isNothing $ String.indexOf (String.Pattern "index.d.ts") fileName) sources
+  let context           =  { sources : filteredSources, types, typeReplacements, regexps, outputRoot }
+  let state             =  mempty
   tuple                 <- Run.runBaseEffect
                             $ State.runState state
                             $ Reader.runReader context write
+  let _ = spy "state" tuple
   pure unit
 
 write :: ReactWriter Unit
 write = do
   { sources } <- Reader.ask
-  traverse_ importsForSource sources
-  writeJavascript
+  traverse_ writeSource sources
+
+
+writeSource :: DeclarationSourceFile -> ReactWriter Unit
+writeSource source = do
+  importsForSource source
+  javascriptExports source
+  setPathAndModuleName source
+  writeJavaScriptFile source
+
 
 importsForSource :: DeclarationSourceFile -> ReactWriter Unit
 importsForSource source = do
   javascriptImports source 
   purescriptImports source
-  javascriptExports source
   pure unit
+
+writeJavaScriptFile :: DeclarationSourceFile -> ReactWriter Unit
+writeJavaScriptFile (DeclarationSourceFile { fileName }) = do
+  psjs <- getPSJS fileName
+  let body = Array.intercalate "\n" psjs.javascript.imports
+  Run.liftEffect $ FS.writeTextFile UTF8 psjs.javascript.path body 
+
+setPathAndModuleName :: DeclarationSourceFile -> ReactWriter Unit 
+setPathAndModuleName (DeclarationSourceFile { fileName }) = do
+  { regexps, outputRoot } <- Reader.ask
+  let match       =  Regex.match regexps.codegenPath fileName 
+  let splitter    =  String.split (String.Pattern "/")
+  tokens          <- Array.nubEq <$> (traverse upperCaseFirst $ maybe [] splitter $ preview (_Just <<< ix 1 <<< _Just) match)
+  let pathPrefix  =  Array.intercalate "/" tokens
+  jsPath          <- Run.liftEffect $ Path.resolve [outputRoot] $ pathPrefix <> ".js"
+  psPath          <- Run.liftEffect $ Path.resolve [outputRoot] $ pathPrefix <> ".purs"
+  let moduleName  =  "React.Basic.MUI." <> (Array.intercalate "." tokens) 
+  psjs          <- getPSJS fileName 
+  setPSJS (psjs { javascript = psjs.javascript { path = jsPath }, purescript = psjs.purescript { path = psPath, moduleName = moduleName } })
+  Run.liftEffect $ log (Array.intercalate "\n" [jsPath, psPath, moduleName])
+ 
 
 javascriptImports :: DeclarationSourceFile -> ReactWriter Unit
 javascriptImports (DeclarationSourceFile { fileName, elements }) | not $ isMUIFile fileName = pure unit 
@@ -172,7 +208,18 @@ purescriptImports (DeclarationSourceFile { fileName, elements }) = do
       { types } <- Reader.ask
       pure $ isJust $ Object.lookup name types 
 
-     
+lowerCaseFirst :: String -> ReactWriter String
+lowerCaseFirst str = do
+  { regexps } <- Reader.ask
+  let replaced = Regex.replace' regexps.firstChar (\s _ -> String.toLower s) str
+  pure replaced
+
+upperCaseFirst :: String -> ReactWriter String
+upperCaseFirst str = do
+  { regexps } <- Reader.ask
+  let replaced = Regex.replace' regexps.firstChar (\s _ -> String.toUpper s) str
+  pure replaced
+    
 
 showEntityName :: EntityName -> String
 showEntityName (Identifier name) = name
