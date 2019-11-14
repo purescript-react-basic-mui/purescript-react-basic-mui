4beff6b82f09f679a0393357a3ddb78897ffea66
Dustin Whitney <dustin.whitney@gmail.com>
2019-11-14 11:32:12 -0500

    added Icons.Types module back

---
+++ src/MUI/Core/Icon/Types.purs
@@ -0,0 +1,38 @@
+module MUI.Icons.Types
+  ( icon
+  , iconWithStyles
+  , Icon
+  )
+  where
+
+import MUI.Core.Styles.CreateMuiTheme (Theme)
+import MUI.Core.Styles.WithStyles (withStyles)
+import MUI.Core.SvgIcon (SvgIconClassKeyOptionsJSS)
+import Prim.Row (class Union)
+import React.Basic (JSX, ReactComponent, element)
+import React.Basic.DOM.Internal (SharedSVGProps)
+import React.Basic.DOM.SVG (Props_svg)
+import Unsafe.Coerce (unsafeCoerce)
+
+newtype Icon = Icon (∀ props. ReactComponent props)
+
+toComponent :: ∀ props. Icon -> ReactComponent props
+toComponent (Icon c) = c
+
+icon
+  :: ∀ given required
+. Union given required ( | SharedSVGProps Props_svg )
+  => Icon
+  -> Record given
+  -> JSX
+icon (Icon i) = element i
+
+iconWithStyles :: ∀ jss jss_ required given
+  . Union given required ( | SharedSVGProps Props_svg )
+  => Union jss jss_ SvgIconClassKeyOptionsJSS
+  => Icon
+  -> (Theme -> Record jss)
+  -> Record given
+  -> JSX
+iconWithStyles (Icon i) style = element (unsafeCoerce withStyles style i)
+
