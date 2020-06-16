6f232e0e74e896fe8fefef2c76567249d9ec6a8a
Tomasz Rybarczyk <paluho@gmail.com>
2020-06-16 16:03:11 +0200

    Preparing to drop latest version of Monomorphic

--- codegen/src/Codegen/AST/Monomorphic.purs
+++ codegen/src/Codegen/AST/Monomorphic.purs
@@ -5,7 +5,6 @@ import Prelude
 import Codegen.AST (ExprF(..), RowF(..), Type, TypeF(..))
 import Codegen.AST.Printers (PrintingContext(..)) as Printers
 import Codegen.AST.Printers (printType)
-import Codegen.AST.Types (recordField)
 import Control.Monad.Error.Class (throwError)
 import Data.Array (uncons) as Array
 import Data.Either (Either)
@@ -26,7 +25,7 @@ build = case _ of
       throwError
         $ "Unable to unify types in application: "
         <> "("
-        <> cata printType (roll x) Printers.StandAlone
+        <> cataM printType (roll x) Printers.StandAlone
         <> ") "
         <> "("
         <> cata printType (roll y) Printers.StandAlone
@@ -44,5 +43,5 @@ build = case _ of
     throwError
       $ "Unable to handle polymorphic variable"
   ExprNumber n -> pure $ TypeNumber
-  ExprRecord props -> pure $ TypeRecord (Row { labels: map (recordField false <<< roll) props, tail: Nothing })
+  ExprRecord props -> pure $ TypeRecord (Row { labels: map roll props, tail: Nothing })
   ExprString s -> pure $ TypeString
