16697fdd5f557bd53a0e85e4f2cbc035c59eaaf9
Tomasz Rybarczyk <paluho@gmail.com>
2020-06-23 23:11:28 +0200

    Trivial purty integration

--- bin/codegen.sh
+++ bin/codegen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-spago run --node-args "codegen --all-components" --config codegen.dhall
+spago run --node-args "codegen --all-components" --config "codegen/spago.dhall"
 
 find ./src -name '*.purs' -exec purty --write '{}' \;
