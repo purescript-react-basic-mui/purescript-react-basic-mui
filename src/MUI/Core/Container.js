f4b6eccf50aeaf231cfa09aae70635652a4d6eda
Tomasz Rybarczyk <paluho@gmail.com>
2020-07-21 09:56:33 +0200

    Add link and conatainer components

---
+++ src/MUI/Core/Container.js
@@ -0,0 +1,3 @@
+// This module was autogenerate. Please don't edit.
+
+exports._UnsafeContainer = require("@material-ui/core/Container").default;
---
+++ src/MUI/Core/Link.js
@@ -0,0 +1,3 @@
+// This module was autogenerate. Please don't edit.
+
+exports._UnsafeLink = require("@material-ui/core/Link").default;
