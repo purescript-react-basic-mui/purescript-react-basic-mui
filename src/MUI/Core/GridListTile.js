4d35594a9c07372227b200fa580622aea8ff1b28
Tomasz Rybarczyk <paluho@gmail.com>
2020-12-04 19:04:16 +0100

    Codegen

---
+++ src/MUI/Core/GridListTile.js
@@ -0,0 +1,3 @@
+// This module was autogenerate. Please don't edit.
+
+exports._UnsafeGridListTile = require("@material-ui/core/GridListTile").default;
---
+++ src/MUI/Lab/Pagination.js
@@ -0,0 +1,3 @@
+// This module was autogenerate. Please don't edit.
+
+exports._UnsafePagination = require("@material-ui/lab/Pagination").default;
